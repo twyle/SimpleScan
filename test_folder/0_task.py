5ade2144acd6bc04e0fe3ab409407d352ffdcf2a
twyle <lyceokoth@gmail.com>
2023-04-06 20:39:22 +0300

    creates the random_number_generator.

--- test_folder/0_task.py
+++ test_folder/0_task.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+import random
+
+
+def generate_two_numbers() -> int:
+    """Generate two random integers between 0 and 5.
+
+    Returns: tuple[int, int]
+        A tuple of two integers.
+    """
+    first_int: int = random.randint(0, 6)
+    second_int: int = random.randint(0, 6)
+    return (first_int, second_int)
