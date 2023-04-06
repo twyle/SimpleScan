8d6edc549a6a28a60ed5954bf645cb0ea804a4fd
twyle <lyceokoth@gmail.com>
2023-04-06 21:12:02 +0300

    adds the gitignore file.

--- test_folder/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module docstring."""
-from task import generate_two_numbers, add_two_numbers
-
-if __name__ == '__main__':
-    int_one, int_two = generate_two_numbers()
-    int_sum = add_two_numbers(int_one, int_two)
-    print(int_sum)
